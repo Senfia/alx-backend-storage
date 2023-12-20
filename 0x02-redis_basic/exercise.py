50ce552b46e7ba78c96dcadac86afd168c935d61
Senyo Fiawornu <contactsenyo@gmail.com>
2023-12-20 17:11:01 +0000

    redis cache class

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
 '''A module for using basic Redis.
 '''
-from functools import wraps
-from typing import Any, Callable, Union
+import uuid
 import redis
+from typing import Union
 
 
-def count_calls(method: Callable) -> Callable:
-    '''Tracks the number of calls made to a method in a Cache class.
-    '''
-    @wraps(method)
-    def wrapper_func(self, *args, **kwargs) -> Any:
-        '''returns method with an incremented call counter.
+class Cache:
+    def __init__(self, redis_url: str = 'redis://localhost:6379/0'):
+        self._redis = redis.Redis.from_url(redis_url)
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        '''takes a data argument and returns a string
         '''
-        if isinstance(self._redis, redis.Redis):
-            self._redis.incr(method.__qualname__)
-        return method(self, *args, **kwargs)
-    return wrapper_func
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
