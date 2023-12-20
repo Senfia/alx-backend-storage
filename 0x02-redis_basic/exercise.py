adf845a91f7fc08163651dad49b4e6c57dac190c
Senyo Fiawornu <contactsenyo@gmail.com>
2023-12-20 17:03:55 +0000

    fixed typo on line 14

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -14,9 +14,7 @@ def count_calls(method: Callable) -> Callable:
     def wrapper_func(self, *args, **kwargs) -> Any:
         '''returns method with an incremented call counter.
         '''
-
         if isinstance(self._redis, redis.Redis):
             self._redis.incr(method.__qualname__)
         return method(self, *args, **kwargs)
-
     return wrapper_func
